dedd92f07e47d536b11ea054955af3e56b013f6d
Mathis Hoffmann <mathis.hoffmann@fau.de>
2019-12-20 13:55:18 +0100

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,8 +1,16 @@
 import setuptools
 
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
 setuptools.setup(
     name = 'pvinspect',
     version = '0.0.1',
+    author = 'Mathis Hoffmann',
+    author_email = 'mathis.hoffmann@fau.de',
+    description = 'Provides methods for the analysis of PV modules using different modailities',
+    long_description=long_description,
+    long_description_content_type="text/markdown",
     url = 'https://github.com/ma0ho/pvinspect',
     packages = setuptools.find_packages(),
     python_requires = '>=3.6, <3.8',
@@ -20,5 +28,10 @@ setuptools.setup(
         'requests',
         'googledrivedownloader',
         'jupyterlab'
-    ]
+    ],
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: Apache License",
+        "Operating System :: OS Independent",
+    ],
 )
