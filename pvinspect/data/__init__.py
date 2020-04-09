1b313c94ecc7269089b2a5f8e7e8766b3b0379ba
Mathis Hoffmann <mathis.hoffmann@fau.de>
2020-04-09 12:00:06 +0200

    Add imports from image back

--- pvinspect/data/__init__.py
+++ pvinspect/data/__init__.py
@@ -3,7 +3,18 @@
 from . import datasets
 from . import io
 from . import image
-from .image import EL_IMAGE, PL_IMAGE, _register_default_plugins
+from .image import (
+    EL_IMAGE,
+    PL_IMAGE,
+    _register_default_plugins,
+    Image,
+    ImageSequence,
+    ModuleImage,
+    ModuleImageSequence,
+    PartialModuleImage,
+    CellImage,
+    CellImageSequence,
+)
 
 __all__ = [
     "EL_IMAGE",
