2a38908b0cce3592bfe78bd429bb5827e26f59d1
Mathis Hoffmann <mathis.hoffmann@fau.de>
2020-07-03 10:50:43 +0200

    Automatically include logits + probs in prediction results

--- pvinspect/common/onnxmodel.py
+++ pvinspect/common/onnxmodel.py
@@ -14,18 +14,24 @@ class ONNXModel:
         classes: List[str],
         logistic_transform: bool,
         input_shape: Tuple[int, int],
-        predictions_postfix: str = "_prediction",
+        probability_postfix: str = "_p_predicted",
+        prediction_postfix: str = "_predicted",
     ):
         self._classes = classes
         self._logistic_transform = logistic_transform
         self._input_shape = input_shape
-        self._predictions_postfix = predictions_postfix
+        self._probability_postfix = probability_postfix
+        self._prediction_postfix = prediction_postfix
 
         # init session
         self._session = onnxruntime.InferenceSession(file)
 
     def predict(
-        self, images: ImageOrSequence, norm_mean: float = None, norm_std: float = None
+        self,
+        images: ImageOrSequence,
+        norm_mean: float = None,
+        norm_std: float = None,
+        thresh: float = 0.5,
     ) -> ImageOrSequence:
 
         if (norm_mean is None or norm_std is None) and len(images) < 20:
@@ -33,7 +39,7 @@ class ONNXModel:
                 "Please provide a sufficient amount of images to automatically compute statistics"
             )
         elif norm_mean is None or norm_std is None:
-            # automatically compute statistics using first 20 images
+            # automatically compute statistics using all images
             imgs = np.stack(
                 [images[i].data.astype(np.float) for i in range(len(images))]
             )
@@ -62,9 +68,15 @@ class ONNXModel:
             if self._logistic_transform:
                 pred = special.expit(pred)
 
-            return {
-                k + self._predictions_postfix: pred[i]
+            d1 = {
+                k + self._probability_postfix: pred[i]
                 for k, i in zip(self._classes, range(len(self._classes)))
             }
+            d2 = {
+                k + self._prediction_postfix: pred[i] > thresh
+                for k, i in zip(self._classes, range(len(self._classes)))
+            }
+            d1.update(d2)
+            return d1
 
-        return images.meta_from_fn(apply)
+        return images.meta_from_fn(apply, progress_bar=True)
