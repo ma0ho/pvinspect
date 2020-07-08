99526b4b9a5dcec1589adb40b8ed6391f28f6cf3
Mathis Hoffmann <mathis.hoffmann@fau.de>
2020-07-08 10:57:06 +0200

    Rework onnxmodel

--- pvinspect/common/onnxmodel.py
+++ pvinspect/common/onnxmodel.py
@@ -5,33 +5,30 @@ from typing import List, Tuple
 from pvinspect.data.image import ImageOrSequence, _sequence, Image
 from skimage.transform import rescale
 from pathlib import Path
+from abc import ABC, abstractmethod
 
 
-class ONNXModel:
+class BaseModel(ABC):
     def __init__(
-        self,
-        file: Path,
-        classes: List[str],
-        logistic_transform: bool,
-        input_shape: Tuple[int, int],
-        probability_postfix: str = "_p_predicted",
-        prediction_postfix: str = "_predicted",
+        self, input_shape: Tuple[int, int],
     ):
-        self._classes = classes
-        self._logistic_transform = logistic_transform
         self._input_shape = input_shape
-        self._probability_postfix = probability_postfix
-        self._prediction_postfix = prediction_postfix
 
+    @abstractmethod
+    def predict(
+        self, images: ImageOrSequence, norm_mean: float = None, norm_std: float = None,
+    ) -> ImageOrSequence:
+        pass
+
+
+class ONNXModel(BaseModel):
+    def __init__(self, file: Path, *argv, **kwargs):
         # init session
         self._session = onnxruntime.InferenceSession(file)
+        super().__init__(*argv, **kwargs)
 
     def predict(
-        self,
-        images: ImageOrSequence,
-        norm_mean: float = None,
-        norm_std: float = None,
-        thresh: float = 0.5,
+        self, images: ImageOrSequence, norm_mean: float = None, norm_std: float = None,
     ) -> ImageOrSequence:
 
         if (norm_mean is None or norm_std is None) and len(images) < 20:
@@ -62,7 +59,42 @@ class ONNXModel:
             # predict
             data = np.tile(data, (1, 3, 1, 1)).astype(np.float32)
             ort_inputs = {self._session.get_inputs()[0].name: data}
-            pred = self._session.run(None, ort_inputs)[0].flatten()
+            pred = self._session.run(None, ort_inputs)[0]
+            return {"prediction": pred}
+
+        return images.meta_from_fn(apply, progress_bar=True)
+
+
+class ClassificationModel:
+    def __init__(
+        self,
+        base: BaseModel,
+        classes: List[str],
+        logistic_transform: bool,
+        probability_postfix: str = "_p_predicted",
+        prediction_postfix: str = "_predicted",
+    ):
+        self._base = base
+        self._classes = classes
+        self._logistic_transform = logistic_transform
+        self._probability_postfix = probability_postfix
+        self._prediction_postfix = prediction_postfix
+
+    def predict(
+        self,
+        images: ImageOrSequence,
+        norm_mean: float = None,
+        norm_std: float = None,
+        thresh: float = 0.5,
+    ) -> ImageOrSequence:
+
+        # run base model
+        images = self._base.predict(
+            images=images, norm_mean=norm_mean, norm_std=norm_std
+        )
+
+        def apply(x: Image):
+            pred = x.get_meta("prediction").flatten()
 
             # sigmoid
             if self._logistic_transform:
@@ -79,4 +111,4 @@ class ONNXModel:
             d1.update(d2)
             return d1
 
-        return images.meta_from_fn(apply, progress_bar=True)
+        return images.meta_from_fn(apply, progress_bar=False)
