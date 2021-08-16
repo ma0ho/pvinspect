4ebe6342b5b71d75f3c6df8dabb8345f21d2358f
Mathis Hoffmann <mathis.hoffmann@fau.de>
2021-08-16 17:02:02 +0200

    Multimodule visualization

--- pvinspect/preproc/default_plugins.py
+++ pvinspect/preproc/default_plugins.py
@@ -1,4 +1,5 @@
 import numpy as np
+from matplotlib import pyplot as plt
 from matplotlib.axes import Axes
 from pvinspect.common.transform import Transform
 from pvinspect.data.image import Image
@@ -37,5 +38,87 @@ class ShowCellCrossings(ShowPlugin):
         )
 
 
+class ShowMultimoduleDetections(ShowPlugin):
+    def __init__(self, title: str = "multimodule detection", priority: int = -50):
+        super().__init__(title, priority=priority)
+
+    def apply(
+        self,
+        ax: Axes,
+        image: Image,
+        multimodule_show_numbers: bool = True,
+        multimodule_highlight_selection: bool = True,
+        multimodule_numbers_fontsize: int = 20,
+        multimodule_show_boxes: bool = True,
+        multimodule_boxes_linewidth: int = 2,
+        **kwargs
+    ) -> None:
+        """Highlight detected modules from multimodule detection
+
+        Args:
+            multimodule_show_numbers (bool): Show the numbers on original multimodule images after detection
+            multimodule_highlight_selection (bool): Highlight the current instance on the multimodule image
+            multimodule_numbers_fontsize (int): Fonsize of multimodule numbes
+            multimodule_show_boxes (bool): Show boxes surrounding detected modules
+            multimodule_boxes_linewidth (bool): Line width of displayed boxes
+        """
+        super().apply(ax, image, **kwargs)
+
+        if (
+            multimodule_show_boxes
+            and isinstance(image, Image)
+            and image.has_meta("multimodule_boxes")
+        ):
+            for i, box in enumerate(image.get_meta("multimodule_boxes")):
+                color = (
+                    "red"
+                    if i == image.get_meta("multimodule_index")
+                    and multimodule_highlight_selection
+                    else "yellow"
+                )
+                ax.plot(
+                    *box[1].exterior.xy,
+                    linewidth=multimodule_boxes_linewidth,
+                    color=color,
+                )
+
+        if (
+            multimodule_show_numbers
+            and isinstance(image, Image)
+            and image.has_meta("multimodule_boxes")
+        ):
+            for i, box in enumerate(image.get_meta("multimodule_boxes")):
+                bgcolor = (
+                    "red"
+                    if i == image.get_meta("multimodule_index")
+                    and multimodule_highlight_selection
+                    else "white"
+                )
+                textcolor = (
+                    "white"
+                    if i == image.get_meta("multimodule_index")
+                    and multimodule_highlight_selection
+                    else "black"
+                )
+                ax.text(
+                    box[1].centroid.x,
+                    box[1].centroid.y,
+                    s=str(i),
+                    color=textcolor,
+                    fontsize=multimodule_numbers_fontsize,
+                    bbox=dict(facecolor=bgcolor, alpha=0.8),
+                    ha="center",
+                    va="center",
+                )
+
+    def is_active(self, image: Image) -> bool:
+        super().is_active(image)
+
+        return image.has_meta("multimodule_boxes") and image.has_meta(
+            "multimodule_index"
+        )
+
+
 def _register_default_plugins():
     register_show_plugin("show_cell_crossings", ShowCellCrossings())
+    register_show_plugin("multimodule_results", ShowMultimoduleDetections())
