48475c8b1ca46fd0f0e342d1e3d9f6bba4cf7d75
Mathis Hoffmann <mathis.hoffmann@fau.de>
2021-08-17 16:17:50 +0200

    Add test

--- pvinspect/analysis/__init__.py
+++ pvinspect/analysis/__init__.py
@@ -1,5 +1,4 @@
-import defects
-import factory_models
-from default_plugins import _register_default_plugins
+from . import defects, factory_models
+from .default_plugins import _register_default_plugins
 
 _register_default_plugins()
